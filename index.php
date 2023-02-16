144e571d73f60aa93dd1997ce781e7cc6fc9edb6
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-16 17:42:26 +0100

    page title added

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ if(isset($_GET['passwordLength'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Strong Password Generator</title>
 </head>
 <body>
 
@@ -33,7 +34,7 @@ if(isset($_GET['passwordLength'])){
         <input name="passwordLength" type="number">
     </div>
     <div  class="py-2">
-        <button type="submit" class="btn btn-primary me-3">Invia</button>
+        <button type="submit" class="btn btn-primary">Invia</button>
     </div>
 </form>
 
